1ce1f8e4aa0a793678a808d5f21a976f145193cc
Michael Wachsman <wachsmanm@gmail.com>
2020-03-03 12:33:12 -0800

    Pressure sensor

--- src/main/java/frc/robot/Shuffle.java
+++ src/main/java/frc/robot/Shuffle.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shuffle {
@@ -9,10 +10,10 @@ public class Shuffle {
   }
 
   public void Update() {
-    //AnalogInput pressure = new AnalogInput(0);
+    AnalogInput pressure = new AnalogInput(0);
     SmartDashboard.putNumber("Shooter RPM", Shooter.getInstance().getShooterRPM());
     SmartDashboard.putNumber("Distance", Limelight.getInstance().getDistance());
-    //SmartDashboard.putNumber("Pressure", ( (pressure.getVoltage()) * 250 / 5.0 - 25.0));
+    SmartDashboard.putNumber("Pressure", ( (pressure.getVoltage()) * 250 / 5.0 - 25.0));
   }
 
   private static class InstanceHolder {
